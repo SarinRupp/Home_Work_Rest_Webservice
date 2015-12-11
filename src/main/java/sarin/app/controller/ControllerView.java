5ed3ab67a3d327720540ab79ee07a2895590a358
SarinRupp <hemsarin.rupp@gmail.com>
2015-12-11 08:09:22 +0700

    heller

---
+++ src/main/java/sarin/app/controller/ControllerView.java
@@ -0,0 +1,30 @@
+package sarin.app.controller;
+
+import java.sql.SQLException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControllerView {
+
+	/*function for load home page*/
+	@RequestMapping(value={"/","/index"}, method = RequestMethod.GET)
+	public ModelAndView list() throws SQLException{			
+		ModelAndView model=new ModelAndView("index");
+		System.out.println(123);
+		return model;
+	}
+	
+	/*function for respone data to view*/
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/help")
+	public ModelAndView help() {			
+		ModelAndView model=new ModelAndView("help");
+		//System.out.println(123);
+		return model;
+	}
+	
+}
